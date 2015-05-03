ba4b9fe540d200b545ec4579ab5b736c67c9b888
zuoxueliang2009 <zuoxueliang2009@126.com>
2015-05-03 20:27:00 +0800

    左学良修改注释

--- WestElectromechanical/WestElectromechanical/classes/WEMine(我的)/WEMineHomeVC(我的首页)/WEMineHomeVC.m
+++ WestElectromechanical/WestElectromechanical/classes/WEMine(我的)/WEMineHomeVC(我的首页)/WEMineHomeVC.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     /**
-     *  我很帅气
+     *  我很帅气左学良
      *
      *  @return <#return value description#>
      */
