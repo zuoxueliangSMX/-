43ab28c3c7e9e6fc05e28514e43766fb68aeadd8
夏亚峰 <80398694@qq.com>
2015-05-12 08:42:47 +0800

    nothing

--- WestElectromechanical/WestElectromechanical/classes/个人中心 /AddAdressVC.m
+++ WestElectromechanical/WestElectromechanical/classes/个人中心 /AddAdressVC.m
@@ -257,7 +257,7 @@ UITextField *phoneNumTF,*streetAdressTF,*emadilCodeTf,*valCodeTf,*userNameTf,*ad
 {
     
     if (self.am) {
-        [we  executeUpdateAdressTaskWithUserId:@"4516" withUserName:userNameTf.text withMobile:phoneNumTF.text withAddress:addressTf.text withDoorPlate:streetAdressTF.text withPostalcode:emadilCodeTf.text withPhone:nil withAdressHandleId:self.am.a_id Success:^(id obj) {
+        [we  executeUpdateAdressTaskWithUserId:@"4516" withUserName:userNameTf.text withMobile:phoneNumTF.text withAddress:addressTf.text withDoorPlate:@"阿拉伯" withPostalcode:@"466200"withPhone:@"15515717255" withAdressHandleId:self.am.a_id Success:^(id obj) {
             
             DLog(@"输出我的修改地址信息%@",obj);
             
